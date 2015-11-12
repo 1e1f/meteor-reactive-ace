3e529abee69f5f9b996fdc1846d1525d3dc9c811
structuresound <leif@structuresound.com>
2015-11-11 18:57:45 -0800

    package

--- package.js
+++ package.js
@@ -4,9 +4,9 @@ Package.describe({
   name: 'chroma:reactive-ace',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'template for attaching ACE editor to a collection object / field',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/structuresound/meteor-reactive-ace',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
